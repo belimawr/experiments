c51611ad6a14a53181634f26114555d308fb6e96
Tiago Queiroz <contato@tiago.eti.br>
2016-10-12 23:01:13 -0300

    tests.sh returns the exitcode of 'go build'

--- GoCD/tests.sh
+++ GoCD/tests.sh
@@ -9,10 +9,16 @@ cd src/GoCD/
 
 go test -coverprofile=reports/coverage.out
 go tool cover -html=reports/coverage.out -o reports/coverage.html
-go test -v | go-junit-report > reports/report.xml
+go test -v > ./test.tmp
+
+ret_code=$?
+
+cat test.tmp | go-junit-report > reports/report.xml
 
 rm -rf ../../reports
 mv reports ../../
 
 cd ../../
 rm -rf ./src/
+
+exit $ret_code
