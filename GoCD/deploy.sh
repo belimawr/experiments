82c9d4c132333372e5a5324b249837b2d207c636
Tiago Queiroz <contato@tiago.eti.br>
2016-10-14 13:09:44 -0300

    Removing application version before creating a new one. Removing debug

--- GoCD/deploy.sh
+++ GoCD/deploy.sh
@@ -2,9 +2,7 @@
 GITHASH=`git rev-parse --short HEAD`
 S3KEY=GoCD-$GITHASH-STG_GoCD.zip
 
-pwd
-ls -la
-
+aws elasticbeanstalk delete-application-version --application-name GoCD --version-label GoCD-$GITHASH-STG_GoCD --delete-source-bundle
 aws s3 cp bin/GoCD_To_Deploy.zip s3://$S3BUCKET/$S3KEY
 aws elasticbeanstalk create-application-version --application-name GoCD --version-label GoCD-$GITHASH-STG_GoCD --source-bundle S3Bucket="$S3BUCKET",S3Key="$S3KEY"
 aws elasticbeanstalk update-environment --environment-name "gocd-env" --version-label GoCD-$GITHASH-STG_GoCD
