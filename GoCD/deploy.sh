e061f75eab13ce1c2584665427d96fa5cbd4d953
Tiago Queiroz <contato@tiago.eti.br>
2016-10-14 11:14:20 -0300

    adding more debug

--- GoCD/deploy.sh
+++ GoCD/deploy.sh
@@ -2,6 +2,9 @@
 GITHASH=`git rev-parse --short HEAD`
 S3KEY=GoCD-$GITHASH-STG_GoCD.zip
 
+pwd
+ls -la
+
 aws s3 cp bin/GoCD-$GITHASH-STG.zip s3://$S3BUCKET/$S3KEY
 aws elasticbeanstalk create-application-version --application-name GoCD --version-label GoCD-$GITHASH-STG_GoCD --source-bundle S3Bucket="$S3BUCKET",S3Key="$S3KEY"
 aws elasticbeanstalk update-environment --environment-name "gocd-env" --version-label GoCD-$GITHASH-STG_GoCD
