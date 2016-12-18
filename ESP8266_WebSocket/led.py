b83f2112f87fbf8dd1884c849d8e23c053832a5a
Tiago Queiroz <contato@tiago.eti.br>
2016-12-18 15:46:42 -0200

    Class implementation, some improvements

--- ESP8266_WebSocket/led.py
+++ ESP8266_WebSocket/led.py
@@ -6,31 +6,38 @@ from machine import Pin
 from neopixel import NeoPixel
 
 
-np = NeoPixel(Pin(4), 24)
+class Led():
+    def __init__(self):
+        self.np = NeoPixel(Pin(4), 16)
+        self.s = None
+        self.ws = None
+        self.client = None
 
-def connect():
-    s = socket.socket()
-    s.bind(('0.0.0.0', 4242))
-    s.listen(1)
-    cl, ip = s.accept()
-    websocket_helper.server_handshake(cl)
-    ws = websocket.websocket(cl, True)
-    return ws
+    def connect(self):
+        self.s = socket.socket()
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.s.bind(('0.0.0.0', 4242))
+        self.s.listen(1)
+        self.client, ip = self.s.accept()
+        self.client.setblocking(False)
+        self.client.setsockopt(socket.SOL_SOCKET, 20, self.read_data)
+        websocket_helper.server_handshake(self.client)
+        self.ws = websocket.websocket(self.client, True)
+        self.ws.write('Connected')
 
+    def read_data(self, *args, **kwargs):
+        msg = self.ws.read()
+        print(msg)
+        if not msg:
+            return 'There is no message to read'
+        split = msg.decode('utf-8').split(':')
+        colours = []
+        for i in split[1].split(','):
+            colours.append(int(i))
 
-def read_data(ws):
-    global np
-    msg = ws.read()
-    print(msg)
-    if not msg:
-        return 'There is no message to read'
-    split = msg.decode('utf-8').split(':')
-    colours = []
-    for i in split[1].split(','):
-        colours.append(int(i))
-
-    for i in range(24):
-        np[i] = colours
-        time.sleep(0.5)
-        np.write()
-    return 'It worked!'
+        for i in range(16):
+            self.np[i] = colours
+            time.sleep(0.1)
+            self.np.write()
+            print('.')
+        return 'It worked!'
