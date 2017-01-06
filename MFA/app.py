31abb18b68fc15dca7cf04f21e9991153e6f5436
Tiago Queiroz <contato@tiago.eti.br>
2017-01-05 23:45:59 -0200

    init MFA using Flask and TOTP

---
+++ MFA/app.py
@@ -0,0 +1,88 @@
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from flask import Flask, render_template, redirect, url_for, flash, session, abort
+from flask.ext.login import LoginManager, UserMixin, login_user, logout_user, current_user
+from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.wtf import Form
+
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import Required, Length, EqualTo
+
+
+app = Flask(__name__)
+
+db = SQLAlchemy(app)
+
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True)
+    password_hash = db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not a readble attribute')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def verify_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
+
+class RegisterForm(Form):
+    username = StringField('Username', validators=[Required(), Length(1, 64)])
+    password = PasswordField('Password', validators=[Required()])
+    password_again = PasswordField('Password again',
+                                   validators=[
+                                       Required(),
+                                       EqualTo('password')])
+    subimit = SubmitField('Register')
+
+
+class LoginForm(Form):
+    username = StringField('Username', validators=[Required(), Length(1, 64)])
+    password = PasswordField('Password', validators=[Required()])
+    subimit = SubmitField('Register')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    """User registration route."""
+    if current_user.is_authenticated():
+        return redirect(url_for('index'))
+
+    form = RegisterForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is not None:
+            flash('Username already exists')
+            return redirect(url_for('register'))
+        user = User(username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+
+        session['username'] = user.username
+        return redirect(url_for('two_factor_setup'))
+    return render_template('register.html', form=form)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated():
+        return redirect(url_for('index'))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user is None or not user.verify_password(form.password.data):
+            flash('Invalid username or password')
+            return redirect(url_for('login'))
+        login_user(user)
+        flash('You are logged in!')
+        return redirect(url_for('index'))
+    return render_template('login.html', form=form)
