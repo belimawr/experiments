1c037907333dba3d1725a35b1f383694b9b9acbe
Tiago Queiroz <contato@tiago.eti.br>
2017-02-18 12:30:44 -0200

    Huge refactor, adding one more test

--- TestHttpClient/src/test/kotlin/demo/TestMyKttpClient.kt
+++ TestHttpClient/src/test/kotlin/demo/TestMyKttpClient.kt
@@ -4,6 +4,7 @@ import com.sun.net.httpserver.HttpExchange
 import com.sun.net.httpserver.HttpHandler
 import com.sun.net.httpserver.HttpServer
 import khttp.get
+import org.junit.After
 import org.junit.Test
 import java.io.IOException
 import java.net.InetSocketAddress
@@ -12,30 +13,52 @@ import kotlin.test.assertEquals
 
 class TestMyKttpClient {
 
+    val server: HttpServer
+    val serverAddr: String
 
-//    internal data class Count(var testCount: Int, var count2: Int)
-
-    public var count = 0
-
-    @Test fun shouldMakeHttpCall() {
-        val server = HttpServer.create(InetSocketAddress(8888), 0)
-        server.createContext("/test", MyHandler(this))
+    init {
+        server = HttpServer.create(InetSocketAddress(8888), 0)
         server.executor = null
         server.start()
 
-        val r = get("http://localhost:8888/test")
+        serverAddr = "http://localhost:" + server.address.port
+    }
+
+    @After
+    fun tearDown() {
+        server.stop(0)
+    }
+
+    @Test fun shouldMakeTwoHTTPCalls() {
+        val counter = Count(0)
+        server.createContext("/shouldMakeTwoHTTPCalls", MockHTTPHandler(counter))
+
+        val r = get(serverAddr + "/shouldMakeTwoHTTPCalls")
+        val r2 = get(serverAddr + "/shouldMakeTwoHTTPCalls")
+
+        assertEquals(2, counter.testCount)
+
         assertEquals(200, r.statusCode)
         assertEquals("This is the response", r.text)
 
-        val r2 = get("http://localhost:8888/notFound")
-        assertEquals(404, r2.statusCode)
-
-        assertEquals(1, count)
+        assertEquals(200, r2.statusCode)
     }
 
-    internal class MyHandler(t: TestMyKttpClient) : HttpHandler {
+    @Test fun shouldMakeOneHTTPCalls() {
+        val counter = Count(0)
+        server.createContext("/test", MockHTTPHandler(counter))
 
-        val myt: TestMyKttpClient = t
+        val r = get(serverAddr + "/test")
+        assertEquals(200, r.statusCode)
+        assertEquals("This is the response", r.text)
+
+        val r2 = get(serverAddr + "/some/other/path")
+        assertEquals(404, r2.statusCode)
+
+        assertEquals(1, counter.testCount)
+    }
+
+    internal class MockHTTPHandler(val counter: Count) : HttpHandler {
 
         @Throws(IOException::class)
         override fun handle(t: HttpExchange) {
@@ -44,8 +67,9 @@ class TestMyKttpClient {
             val os = t.responseBody
             os.write(response.toByteArray())
             os.close()
-            myt.count++
+            counter.testCount++
         }
     }
 
+    data class Count(var testCount: Int)
 }
