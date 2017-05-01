0011a7252ca84144d525d8e34ba0baa68b20c351
Tiago Queiroz <contato@tiago.eti.br>
2017-05-01 19:59:25 -0300

    Identifying quite good lines

--- OpenCV-Python/picamera-lineFinder.py
+++ OpenCV-Python/picamera-lineFinder.py
@@ -1,13 +1,10 @@
-#Sources: http://picamera.readthedocs.io/en/release-1.6/recipes1.html#capturing-to-an-opencv-object
-#         https://raspberrypi.stackexchange.com/q/22241
-
 import io
+import math
 
 import picamera
 import cv2
 
 import numpy as np
-
 
 vertices = np.array([[0,0],[0,320], [640,320], [640,0]], np.int32)
 
@@ -16,25 +13,30 @@ def roi(image):
 	cv2.fillPoly(mask, [vertices], 255)
 	masked = cv2.bitwise_and(image, mask)
 	return masked
-	
-	
+
 def process_image(image):
 	img = cv2.Canny(image, threshold1=100, threshold2=200)
-	#HoughLinesP(image, rho, theta, threshold[, lines[, minLineLength[, maxLineGap]]]) -> lines
-	lines = cv2.HoughLinesP(img, 1, np.pi/180, 180, 20, 15)
-	draw_lines(img, lines)
-	return img
+	img = cv2.GaussianBlur(img, (3,3), 0 )
+	lines = cv2.HoughLinesP(img, 1, np.pi/180, 180, minLineLength=150, maxLineGap=10)
+	return img, lines
 
 def draw_lines(img, lines):
 	try:
-		cv2.line(img, (0,0), (640,480), [0,0,255], 15)
-		for line in lines:
+		for i, line in enumerate(lines):
 			coords = line[0]
-			print(coords)
-			cv2.line(img, (coords[0],coords[1]), (coords[2],coords[3]), [0,0,255], 15)
-	except:
-			cv2.line(img, (0,0), (640,480), (0,0,255), 15)
+			if coords[1] >= 315:
+				continue
+
+			print(i, angle(coords))
+			cv2.line(img, (coords[0],coords[1]), (coords[2],coords[3]), 255, 3)
+	except TypeError as e:
+		print('No Line :(')
 	return
+
+def angle(line):
+	x1, y1, x2, y2 = line
+	m = abs(y1 - y2)/abs(x1 - x2)
+	return math.degrees(math.atan(m))
 
 with picamera.PiCamera() as camera:
 	camera.resolution = (640, 480)
@@ -47,12 +49,13 @@ with picamera.PiCamera() as camera:
 		stream.seek(0)
 		
 		frame = cv2.imdecode(frame, cv2.IMREAD_GRAYSCALE)
-		#frame = roi(frame)
-		cv2.imshow('Image- ROI', frame)
+		frame = roi(frame)
 		
-		frame = process_image(frame)
+		processed_img, lines = process_image(frame)
+		draw_lines(frame, lines)
 		
-		cv2.imshow('Image', frame)
+		cv2.imshow('Image - ROI', frame)
+
 		if cv2.waitKey(1) & 0xFF == ord('q'):
 			break
 
